c92176094bb658e2bc00855e9d478c5fc0de8933
Ayrat <ayrat1@mail.ru>
2020-07-27 00:40:36 +0300

    46.00

--- src/main/java/com/example/ConsoleAnnouncer.java
+++ src/main/java/com/example/ConsoleAnnouncer.java
@@ -1,6 +1,8 @@
 package com.example;
 
 public class ConsoleAnnouncer implements Announcer {
+    private Recommendator recommendator = ObjectFactory.getInstance().createObject(Recommendator.class);
+
     @Override
     public void announce(String message) {
         System.out.println(message);
